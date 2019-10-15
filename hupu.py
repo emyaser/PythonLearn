08c2b811e467cb492dcba278efe4f910edbd7ec8
emyaer <emyaser@163.com>
2019-10-15 13:22:14 +0800

    Django学习 基本Web页面搭建

--- hupu.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-start_url='https://bbs.hupu.com/bxj'
-repos = requests.get(start_url).text
-print(repos)
---
+++ oa/hrs/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+class Dept(models.Model):
+    no=models.IntegerField(primary_key=True, db_column='dno', verbose_name='部门编号')
+    name=models.CharField(max_length=20, db_column='dname', verbose_name='部门名称')
+    location=models.CharField(max_length=10, db_column='dloc', verbose_name='部门所在地')
+
+    class Meta:
+        db_table='tb_dept'
+    def __str__(self):
+        return self.name
+
+class Emp(models.Model):
+    no=models.IntegerField(primary_key=True, db_column='eno', verbose_name='员工编号')
+    name = models.CharField(max_length=20, db_column='ename', verbose_name='员工姓名')
+    job = models.CharField(max_length=10, verbose_name='职位')
+    # 自参照完整性多对一外键关联
+    mgr = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='直接主管')
+    sal = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='月薪')
+    comm = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True, verbose_name='补贴')
+    # 多对一外键关联
+    dept = models.ForeignKey(Dept, db_column='dno', on_delete=models.PROTECT, verbose_name='所在部门')
+
+    class Meta:
+        db_table = 'tb_emp'
+    def __str__(self):
+        return self.name
